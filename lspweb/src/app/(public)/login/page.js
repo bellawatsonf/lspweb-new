b406a1f4347885d6d37591b91c69c23638cda3d2
bellawatsonf <bellawatsonf@gmail.com>
2023-07-10 22:48:47 +0700

    react oauth

--- lspweb/src/app/(public)/login/page.js
+++ lspweb/src/app/(public)/login/page.js
@@ -3,28 +3,35 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
+import { googleLogout } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 export default function Login() {
   console.log("masuk1");
-  const responseMessage = (response) => {
-    console.log("masuk2");
 
-    console.log(response, "response");
+  const googleSuccess = (resp) => {
+    let decoded = jwt_decode(resp?.credential);
+    const email = decoded?.email;
+    const name = decoded?.name;
+    const token = resp?.tokenId;
+    const googleId = resp?.googleId;
+    const result = { email, name, token, googleId };
+    // dispatch(googleLogin({ result, navigate, toast }));
+    console.log(result);
   };
-  const errorMessage = (error) => {
-    console.log("masuk3");
-
-    console.log(error, "error");
-  };
-
   return (
     <>
       <a href="/registrasi">registrasi</a>
       <GoogleOAuthProvider clientId="8183620420-97vo1t4g23unvaa2qd97249j98agerul.apps.googleusercontent.com">
         <GoogleLogin
-          onSuccess={() => responseMessage()}
-          onError={() => errorMessage()}
+          onSuccess={(resp) => {
+            googleSuccess(resp);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+          useOneTap
         />
+        ;
       </GoogleOAuthProvider>
     </>
   );
